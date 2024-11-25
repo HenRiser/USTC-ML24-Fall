c2569d3b70d0086638093637bf58ceb3c3e7ead5
HenRiser <lhrc22111606@mail.ustc.edu.cn>
2024-11-25 18:49:13 +0800

    lab2done

--- lab2/bp-algorithm.py
+++ lab2/bp-algorithm.py
@@ -6,47 +6,57 @@ from sklearn import datasets
 from pandas import DataFrame
 import numpy as np
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 iris=datasets.load_iris()
 df=DataFrame(iris.data,columns=iris.feature_names)
 df['target']=list(iris.target)
 X=df.iloc[:,0:4]
 Y=df.iloc[:,4]
-#划分数据
+print(df)
+#对df进行划分，一共五项数据，前四项是特征为x，第五项是tag为y
 
-'''
-在此填入你的代码
-'''
-
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,train_size=0.8,random_state=22111606)#以22111606为随机种子对数据集进行要求比例的可复现随机划分
+#正规化
 sc=StandardScaler()
 sc.fit(X)
 standard_train=sc.transform(X_train)
 standard_test=sc.transform(X_test)
 
+#可视化
+# plt.figure(figsize=(12, 5))
+# plt.subplot(1, 2, 1)
+# plt.scatter(standard_train[:, 0],standard_train[:, 1], c=Y_train, edgecolors='k', marker='o', s=100)
+# plt.title('SEPAL')
+# plt.xlabel('sepal_l')
+# plt.ylabel('sepal_w')
+# plt.grid()
+
+# plt.figure(figsize=(12, 5))
+# plt.subplot(1, 2, 2)
+# plt.scatter(standard_train[:, 2],standard_train[:, 3], c=Y_train, edgecolors='k', marker='o', s=100)
+# plt.title('PETAL')
+# plt.xlabel('petal_l')
+# plt.ylabel('petal_w')
+# plt.grid()
 
 #构建mlp模型
-'''
-在此填入你的代码
-'''
-#拟合数据
-'''
-在此填入你的代码
-'''
-#得到预测结果
-'''
-在此填入你的代码
-'''
+mlp = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=2000, activation='tanh',solver='adam', alpha=0.005, batch_size='auto', learning_rate='constant', learning_rate_init=0.01,shuffle=True, random_state=22111606, tol=0.00001, verbose=False)
 
+mlp.fit(standard_train,Y_train)#用正规化后的数据与tag进行训练
+
+result = mlp.predict(standard_test)#用训练得到的模型和验证集进行计算
 
 
 #查看模型结果
-print("测试集合的y值：",list(Y_test))
-print("神经网络预测的的y值：",list(result))
+print("测试集合的y值   ：",list(Y_test))
+print("神经网络预测的y值：",list(result))
 print("预测的准确率为：",mlp.score(standard_test,Y_test))
 print("层数为：",mlp.n_layers_)
 print("迭代次数为：",mlp.n_iter_)
 print("损失为：",mlp.loss_)
 print("激活函数为：",mlp.out_activation_)
-
 
 #代码的手动实现
 class NeuralNetwork:
@@ -63,30 +73,38 @@ class NeuralNetwork:
        self.bias_hidden = np.zeros((1, self.hidden_size))
        self.bias_output = np.zeros((1, self.output_size))
 
+    #sigmoid(x) = 1/(1+e^-x)
    def sigmoid(self, x): #sigmoid计算方式
-       '''
-       在此填入你的代码
-       '''
-       return
-
+       return 1/(1+np.exp(-x))
+    # sigmoid'(x) = (1-sigmoid(x))sigmoid(x)
    def sigmoid_derivative(self, x): #sigmoid导数计算方式
-       '''
-       在此填入你的代码
-       '''
-       return
+       return (1.0-self.sigmoid(x))*self.sigmoid(x)
 
    def forward(self, X):
-       '''
-       在此填入你的代码
-       '''
-       return
+       a1 = np.dot(X,self.weights_input_hidden) + self.bias_hidden #直接用权重和偏置计算得到的结果
+       z1 = self.sigmoid(a1)    #用sigmoid转化后的概率，下同
+       a2 = np.dot(z1,self.weights_hidden_output) + self.bias_output
+       y  = self.sigmoid(a2)
+       return y
 
    def backward(self, X, y, output, learning_rate):
-       '''
-       在此填入你的代码
-       '''
-
-
+       
+       grads={} #用字典存储对应梯度，，其实可以不用存储而是在最后一步的反向传播中直接现算现用，但是这样子写可读性更好
+       a1 = np.dot(X,self.weights_input_hidden) + self.bias_hidden#这一步很难受，如果把前向和反向放一个方法里面就不用多余的时间空间了
+       dy = (output-y)/X.shape[0]   #预测结果的误差
+       z1 = self.sigmoid(a1)
+       da1 = np.dot(dy,self.weights_hidden_output.T)
+       dz1 = self.sigmoid_derivative(a1) * da1
+       #通过上面的变量计算出梯度
+       grads['weights_hidden_output'] = np.dot(z1.T,dy)
+       grads['bias_output'] = np.sum(dy,axis=0)
+       grads['weights_input_hidden'] = np.dot(X.T,dz1)
+       grads['bias_hidden'] = np.sum(dz1,axis=0)
+       #更新params
+       self.weights_hidden_output -= learning_rate* grads['weights_hidden_output']
+       self.bias_output -= learning_rate* grads['bias_output']
+       self.weights_input_hidden -= learning_rate* grads['weights_input_hidden']
+       self.bias_hidden -= learning_rate* grads['bias_hidden']
 
    def train(self, X, y, epochs, learning_rate):
        for epoch in range(epochs):
@@ -95,9 +113,10 @@ class NeuralNetwork:
            self.backward(X, y, output, learning_rate)
            if epoch % 100 == 0:
                print(f"Epoch {epoch + 1}, Loss: {loss}")
+            #训练epoch次且每一百次输出对应loss
 
    def predict(self, X):
-       return np.round(self.forward(X))
+       return np.round(self.forward(X))#预测结果且取整数
 
 
 # 将标签转换为独热编码
@@ -108,7 +127,6 @@ def one_hot_encode(labels):
        one_hot_labels[i][label] = 1
    return one_hot_labels
 
-
 # 构建神经网络
 input_size = X_train.shape[1]
 hidden_size = 10
@@ -116,11 +134,11 @@ output_size = len(np.unique(Y_train))  # 根据训练集标签确定输出层大
 nn = NeuralNetwork(input_size, hidden_size, output_size)
 
 # 将标签转换为独热编码
-Y_train_encoded = one_hot_encode(Y_train)
+Y_train_encoded = one_hot_encode(Y_train)#用独热码标识正确tag再用于监督学习
 
 # 训练神经网络
 print('training.......')
-nn.train(standard_train, Y_train_encoded, epochs=1000, learning_rate=0.01)
+nn.train(standard_train, Y_train_encoded, epochs=1000, learning_rate=0.1)
 
 # 预测测试集
 predictions = nn.predict(standard_test)
---
+++ lab2/report.md
@@ -0,0 +1,23 @@
+# <center> lab2_report </center>
+## <center> PB22111606_龙涵睿 </center>
+### 1.运行结果
+- 基于sklearn框架的运行结果（特征分布图是自己加的）
+<img src =image.png style = "width:50%;height:auto;">
+- 基于助教框架的运行结果
+![alt text](image-1.png)
+
+由结果可知：
+> TA >= sklearn is True
+
+### 2.实验用时
+我是在kaggle的notebook上完成的，算上在本地环境的七七八八，大概三四个小时吧
+### 3.超参数修改
+- sklearn:
+  `mlp = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=2000, activation='tanh',solver='adam', alpha=0.005, batch_size='auto'，learning_rate='constant', learning_rate_init=0.01,shuffle=True, random_state=22111606, tol=0.00001, verbose=False)`
+- 手动：
+  因为补充完代码后只修改了**learning_rate:0.01 -> 0.1**就已经能够跑到**acc==1**了，所以没有增加正则化等，epoch也是 default——1000，因为数据集是按比例随机划分，所以准确率会在0.9以上有所浮动，1中截图为最佳结果，即一次划分两种方法都是acc==1
+### 4.代码相关
+注释都在代码中，此外，因为readme里面有助教复现代码运行结果的有关要求，~~所以我用*random_state=22111606*来实现一个可复现的运行结果~~实践发现就算我用了同一个random_state也会导致结果出现细微差异，不知道为啥，，，但结果都是在0.9~0.9333...
+`X_train,X_test,Y_train,Y_test=train_test_split(X,Y,train_size=0.8,random_state=22111606)`
+### 5.other
+一开始不是在kaggle_nb上面跑的，而是在本地envs，但是又`ImportError: cannot import name '__version__' from 'numpy' (unknown location)`,很闹麻，这个numpy版本兼容性这么差吗
